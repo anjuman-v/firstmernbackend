f29dd3a00ba01ec8e4fc7e6a044801c276e053b6
Anjuman <ansa.anjuman00786@gmail.com>
2022-05-09 01:18:20 +0530

    1-1

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const connect = () =>{ 
     return mongoose.connect(
-    // 'mongodb+srv://anjuman:anjuman@cluster0.l0hn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    'mongodb://localhost:27017/firstMerndb'
+     'mongodb+srv://anjuman:anjuman@cluster0.l0hn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    // 'mongodb://localhost:27017/firstMerndb'
 )
 }
 
